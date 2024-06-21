045e2cf2b32597d7cd9d2a7f4c320984458320f7
Jodi Giordano <jodi@warezclan.com>
2024-06-21 15:57:46 -0400

    frontend - move system

--- frontend/simulation.ts
+++ frontend/simulation.ts
@@ -12,6 +12,7 @@ import {
   RuntimeLink,
   RuntimeSubsystem,
   RuntimeSystem,
+  SystemMargin,
 } from "@dataflows/spec";
 import {
   SystemSimulator,
@@ -90,6 +91,174 @@ export class CanvasSimulator {
   constructor(system: RuntimeSystem) {
     this.system = system;
     this.systemSimulator = new SystemSimulator(system);
+  }
+
+  moveSystem(system: RuntimeSubsystem, deltaX: number, deltaY: number): void {
+    // Move the ss.
+    const ssPosition = system.specification.position;
+
+    ssPosition.x += deltaX;
+    ssPosition.y += deltaY;
+
+    const centerSS = {
+      x: ssPosition.x + system.size.width / 2,
+      y: ssPosition.y + system.size.height / 2,
+    };
+
+    // Retrieve sibling subsystems and
+    // sort them by distance of ss, nearest first.
+    const subsystems = system.parent?.systems ?? [];
+
+    // Resolve collisions.
+
+    let iterations = 0;
+    const displacedThisIteration: string[] = [];
+
+    do {
+      displacedThisIteration.length = 0;
+      iterations += 1;
+
+      for (const ssACandidate of subsystems) {
+        for (const ssBCandidate of subsystems) {
+          if (
+            displacedThisIteration.includes(
+              [ssACandidate.canonicalId, ssBCandidate.canonicalId].join("."),
+            ) ||
+            ssACandidate.canonicalId === ssBCandidate.canonicalId
+          ) {
+            continue;
+          }
+
+          // Find which subsystem displaces and
+          // which subsystem is being displaced.
+          //
+          // It is important that the order is consistent between iterations.
+          // So if subsystem A displaces subsystem B on iteration 0,
+          // it is important that A still displaces B on iteration 1.
+          //
+          // To accomplish this, we apply this rule: the subsystem which
+          // displaces is always the one nearest (center to center) to the
+          // subsystem being moved (i.e. the first parameter of this function).
+          //
+          // Special case: the subsyssAstem being moved is always displacing.
+          const ssACandidateCenterX =
+            ssACandidate.specification.position.x + ssACandidate.size.width / 2;
+
+          const ssaCandidateCenterY =
+            ssACandidate.specification.position.y +
+            ssACandidate.size.height / 2;
+
+          const ssACandidateDistance = Math.sqrt(
+            Math.pow(ssACandidateCenterX - centerSS.x, 2) +
+              Math.pow(ssaCandidateCenterY - centerSS.y, 2),
+          );
+
+          const ssBCandidateCenterX =
+            ssBCandidate.specification.position.x + ssBCandidate.size.width / 2;
+
+          const ssBCandidateCenterY =
+            ssBCandidate.specification.position.y +
+            ssBCandidate.size.height / 2;
+
+          const ssBCandidateDistance = Math.sqrt(
+            Math.pow(ssBCandidateCenterX - centerSS.x, 2) +
+              Math.pow(ssBCandidateCenterY - centerSS.y, 2),
+          );
+
+          // Subsystem displacing.
+          const ssA =
+            ssACandidate.canonicalId === system.canonicalId ||
+            ssACandidateDistance < ssBCandidateDistance
+              ? ssACandidate
+              : ssBCandidate;
+
+          // Subsystem being displaced.
+          const ssB =
+            ssA.canonicalId === ssACandidate.canonicalId
+              ? ssBCandidate
+              : ssACandidate;
+
+          const aPositionX1 = ssA.specification.position.x - SystemMargin;
+          const aPositionX2 =
+            ssA.specification.position.x + ssA.size.width + SystemMargin;
+          const aPositionY1 = ssA.specification.position.y - SystemMargin;
+          const aPositionY2 =
+            ssA.specification.position.y + ssA.size.height + SystemMargin;
+
+          const bPositionX1 = ssB.specification.position.x - SystemMargin;
+          const bPositionX2 =
+            ssB.specification.position.x + ssB.size.width + SystemMargin;
+          const bPositionY1 = ssB.specification.position.y - SystemMargin;
+          const bPositionY2 =
+            ssB.specification.position.y + ssB.size.height + SystemMargin;
+
+          // Calculate the area of intersection,
+          // which is a rectangle [0, 0, X, Y].
+          const overlapX = Math.max(
+            0,
+            Math.min(aPositionX2, bPositionX2) -
+              Math.max(aPositionX1, bPositionX1),
+          );
+
+          const overlapY = Math.max(
+            0,
+            Math.min(aPositionY2, bPositionY2) -
+              Math.max(aPositionY1, bPositionY1),
+          );
+
+          // No overlap.
+          if (overlapX === 0 || overlapY === 0) {
+            continue;
+          }
+
+          const aCenterX = (aPositionX1 + aPositionX2) / 2;
+          const aCenterY = (aPositionY1 + aPositionY2) / 2;
+
+          let bCenterX = (bPositionX1 + bPositionX2) / 2;
+          const bCenterY = (bPositionY1 + bPositionY2) / 2;
+
+          if (aCenterX === bCenterX && aCenterY === bCenterY) {
+            bCenterX += 1;
+          }
+
+          const centerToCenterMagnitude = Math.sqrt(
+            Math.pow(bCenterX - aCenterX, 2) + Math.pow(bCenterY - aCenterY, 2),
+          );
+
+          const centerToCenterUnitVectorX =
+            (bCenterX - aCenterX) / centerToCenterMagnitude;
+
+          const centerToCenterUnitVectorY =
+            (bCenterY - aCenterY) / centerToCenterMagnitude;
+
+          // TODO:
+          //
+          // find the optimal length of displacement
+          // to reduce the number of iterations.
+          const displacementLength = 1;
+
+          const displacementX =
+            centerToCenterUnitVectorX >= 0
+              ? Math.ceil(centerToCenterUnitVectorX * displacementLength) | 0
+              : Math.floor(centerToCenterUnitVectorX * displacementLength) | 0;
+
+          const displacementY =
+            centerToCenterUnitVectorY >= 0
+              ? Math.ceil(centerToCenterUnitVectorY * displacementLength) | 0
+              : Math.floor(centerToCenterUnitVectorY * displacementLength) | 0;
+
+          ssB.specification.position.x += displacementX;
+          ssB.specification.position.y += displacementY;
+
+          displacedThisIteration.push(
+            [ssA.canonicalId, ssB.canonicalId].join("."),
+          );
+          displacedThisIteration.push(
+            [ssB.canonicalId, ssA.canonicalId].join("."),
+          );
+        }
+      }
+    } while (displacedThisIteration.length && iterations < 1000);
   }
 
   getBoundaries() {
