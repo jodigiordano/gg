3c3ee45cf98343118665eb16d374fbc96f556c69
Jodi Giordano <jodi@warezclan.com>
2024-07-10 22:13:04 -0400

    lint

--- frontend/src/pixi.ts
+++ frontend/src/pixi.ts
@@ -1,19 +1,19 @@
 // From https://pixijs.io/customize/
 
-export * from '@pixi/assets';
-export * from '@pixi/display';
-export * from '@pixi/constants';
-export * from '@pixi/math';
-export * from '@pixi/runner';
-export * from '@pixi/settings';
-export * from '@pixi/ticker';
-import * as utils from '@pixi/utils';
+export * from "@pixi/assets";
+export * from "@pixi/display";
+export * from "@pixi/constants";
+export * from "@pixi/math";
+export * from "@pixi/runner";
+export * from "@pixi/settings";
+export * from "@pixi/ticker";
+import * as utils from "@pixi/utils";
 export { utils };
-export * from '@pixi/core';
-export * from '@pixi/events';
-export * from '@pixi/sprite';
-export * from '@pixi/app';
-export * from '@pixi/graphics';
-export * from '@pixi/sprite-tiling';
-export * from '@pixi/spritesheet';
-export * from '@pixi/text';
+export * from "@pixi/core";
+export * from "@pixi/events";
+export * from "@pixi/sprite";
+export * from "@pixi/app";
+export * from "@pixi/graphics";
+export * from "@pixi/sprite-tiling";
+export * from "@pixi/spritesheet";
+export * from "@pixi/text";
