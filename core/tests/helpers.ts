a869aa71a81f8cead0c26f41be9017070fec656a
Jodi Giordano <jodi@warezclan.com>
2024-07-24 17:52:27 -0400

    gha

--- core/tests/helpers.ts
+++ core/tests/helpers.ts
@@ -1,11 +1,17 @@
 import { readFileSync } from "node:fs";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { loadYaml } from "../src/index";
 
 export function loadExample(name: string) {
+  const examplesDirectory = [
+    dirname(fileURLToPath(import.meta.url)),
+    "..",
+    "examples",
+  ].join("/");
+
   return loadYaml(
-    readFileSync(
-      [import.meta.dirname, `../examples/${name}.yml`].join("/"),
-      "utf8",
-    ),
+    readFileSync([examplesDirectory, `${name}.yml`].join("/"), "utf8"),
   );
 }
