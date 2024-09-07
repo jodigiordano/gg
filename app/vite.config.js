ddb58f013379cbb9627b8f71ca247432e5adf4d3
Jodi Giordano <jodi@warezclan.com>
2024-09-07 10:10:56 -0400

    sitemap.xml & robots.txt

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,4 +1,6 @@
+import { defineConfig, loadEnv } from 'vite';
 import checker from "vite-plugin-checker";
+import sitemap from 'vite-plugin-sitemap'
 import browserslistToEsbuild from "browserslist-to-esbuild";
 import fs from "node:fs";
 
@@ -43,47 +45,64 @@ function partialHtmlLoader() {
   };
 }
 
-export default {
-  root: "src",
-  envDir: "../",
-  cacheDir: "tmp",
-  server: {
-    host: "0.0.0.0",
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        secure: false,
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    root: "src",
+    envDir: "../",
+    cacheDir: "tmp",
+    server: {
+      host: "0.0.0.0",
+      proxy: {
+        "/api": {
+          target: "http://localhost:3000",
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  plugins: [
-    checker({
-      typescript: true,
-    }),
-    base64Loader(),
-    inlineSvgLoader(),
-    partialHtmlLoader(),
-  ],
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-    target: browserslistToEsbuild(),
-    sourcemap: true,
-    assetsInlineLimit: 1024 * 1024, // 1MB.
-    rollupOptions: {
-      input: {
-        editor: "./src/index.html",
-        viewer: "./src/viewer.html",
-        signIn: "./src/sign-in.html",
-        plus: "./src/plus.html",
-        profile: "./src/profile.html",
-        terms: "./src/terms.html",
-        privacy: "./src/privacy.html",
-        graphs: "./src/graphs.html",
-        graph: "./src/graph.html",
-        export: "./src/export.html",
+    plugins: [
+      checker({
+        typescript: true,
+      }),
+      base64Loader(),
+      inlineSvgLoader(),
+      partialHtmlLoader(),
+      sitemap({
+        hostname: process.env["VITE_PUBLIC_URL"],
+        exclude: ["/viewer", "/profile", "/graphs", "/graph", "/export"],
+        changefreq: "weekly",
+        priority: {
+          "/editor": 1.0,
+          "/plus": 0.9,
+          "/sign-in": 0.8,
+          "/terms": 0.7,
+          "/privacy": 0.7,
+        },
+        generateRobotsTxt: true,
+      }),
+    ],
+    build: {
+      outDir: "../dist",
+      emptyOutDir: true,
+      target: browserslistToEsbuild(),
+      sourcemap: true,
+      assetsInlineLimit: 1024 * 1024, // 1MB.
+      rollupOptions: {
+        input: {
+          editor: "./src/index.html",
+          viewer: "./src/viewer.html",
+          signIn: "./src/sign-in.html",
+          plus: "./src/plus.html",
+          profile: "./src/profile.html",
+          terms: "./src/terms.html",
+          privacy: "./src/privacy.html",
+          graphs: "./src/graphs.html",
+          graph: "./src/graph.html",
+          export: "./src/export.html",
+        },
       },
-    },
-  },
-};
+    }
+  });
+ };
