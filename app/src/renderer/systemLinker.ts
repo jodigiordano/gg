86427f9736e5e90f1e011a8e7a06525c1d28a113
Jodi Giordano <giordano.jodi@gmail.com>
2024-10-17 21:27:55 -0400

    Fix selecting a line termination that causes a "move link" (#85)
    * fix
    
    * bump
    
    ---------
    
    Co-authored-by: Jodi Giordano <jodi@warezclan.com>

--- app/src/renderer/systemLinker.ts
+++ app/src/renderer/systemLinker.ts
@@ -4,6 +4,8 @@ import { BlockSize } from "../helpers.js";
 export default class SystemLinker extends Container {
   private line: Graphics;
 
+  public length: number;
+
   constructor() {
     super();
 
@@ -11,6 +13,7 @@ export default class SystemLinker extends Container {
     this.visible = false;
 
     this.line = new Graphics();
+    this.length = 0;
 
     // @ts-ignore
     this.addChild(this.line);
@@ -21,5 +24,7 @@ export default class SystemLinker extends Container {
     this.line.lineStyle(BlockSize / 4, 0xffffff);
     this.line.moveTo((x1 + 0.5) * BlockSize, (y1 + 0.5) * BlockSize);
     this.line.lineTo((x2 + 0.5) * BlockSize, (y2 + 0.5) * BlockSize);
+
+    this.length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
   }
 }
